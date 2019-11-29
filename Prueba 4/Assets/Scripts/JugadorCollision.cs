d6b234679f5f94303b81091c358249afcb31b9b2
sslavik <sslavikshy@gmail.com>
2019-11-29 11:22:22 +0100

    Tema 4 BasicPhysics

---
+++ Prueba 4/Assets/Scripts/JugadorCollision.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JugadorCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// Se produce cuando entra en contacto con otro Collider
+	void OnCollisionEnter(Collision other) {
+		Debug.LogFormat("ENTER: Colisionado con {0}", other.collider.name);
+	}
+	
+	// Se produce cuando la colisión es constante con el otro Collider
+	void OnCollisionStay(Collision other) {
+		Debug.LogFormat("STAY: Colisionado con {0}", other.collider.name);
+
+	}
+
+	// Se produce cuando se termina la colisión con el otro Collider
+	void OnCollisionExit(Collision other) {
+		Debug.LogFormat("EXIT: Colisionado con {0}", other.collider.name);
+
+	}
+
+}
---
+++ Prueba 4/Assets/Scripts/JugadorMover.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JugadorMover : MonoBehaviour {
+
+	//CAMPOS
+	public float fuerza = 30F;
+	public Text mensaje;
+	Rigidbody rigidBody;
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody>();// OBTIENE EL COMPONENTE DEL OBJETO QUE TIENE EL SCRIPTS
+	}
+	
+	// FixedUpdate es recomendable para usarlo con físicas
+	void FixedUpdate () {
+		rigidBody.AddForce(new Vector3(Input.GetAxis("Horizontal")*fuerza, transform.position.y ,Input.GetAxis("Vertical")*fuerza),
+		ForceMode.Force); // AÑADIMOS EL TIPO DE FUERZA
+	}
+
+	void OnCollisionEnter(Collision other){
+		if(other.gameObject.tag == "Enemigo")
+			Destroy(other.gameObject);
+		mensaje.text = other.collider.name + ", Detectado enemigo";
+	}
+
+}
